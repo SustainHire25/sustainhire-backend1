d74293515a075184d5b643610184ac113b69f2fe
SustainHire25 <hira242729@gmail.com>
2025-10-06 21:53:15 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: "User already exists" });
+    if (existingUser) return res.status(400).json({ message: "User already exists Kindly login!" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ email, password: hashedPassword });
